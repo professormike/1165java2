89ab49003a66eacafd42890b1695104c7cf0f692
Michael Burrell <michael.burrell@sheridancollege.ca>
2016-05-09 07:28:49 -0400

    Test initial stupid project

---
+++ TestGitProject/src/testgitproject/TestGitProject.java
@@ -0,0 +1,17 @@
+package testgitproject;
+
+/**
+ *
+ * @author Michael Burrell
+ * @since 2016-05-09
+ */
+public class TestGitProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is a test");
+    }
+    
+}
